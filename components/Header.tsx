05694b4b8d398f938bd21597f9f5f0fd6a2678f4
Byeonghoon Lee <byeonghoonlee@Byeonghoonui-iMac.local>
2021-02-15 12:59:02 +0900

    feat(Header) add Header Component

---
+++ components/Header.tsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import styled from 'styled-components';
+import Link from 'next/link';
+
+import AirbnbLogoIcon from '../public/static/svg/logo.svg';
+import AirbnbLogoTextIcon from '../public/static/svg/logo_text.svg';
+import palette from '../styles/palette';
+import SignUpModal from './SignUpModal';
+import useModal from '../hooks/useModal';
+
+const Container = styled.div`
+  position: sticky;
+  top: 0;
+  width: 100%;
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 80px;
+  background-color: white;
+  box-shadow: rgba(0, 0, 0, 0.08) 0 1px 12px;
+  z-index: 10;
+
+  .header-logo-wrapper {
+    display: flex;
+    align-items: center;
+
+    .header-logo {
+      margin-right: 6px;
+    }
+  }
+
+  .header-auth-buttons {
+    display: flex;
+    align-items: center;
+
+    .header-button {
+      height: 42px;
+      padding: 0 16px;
+      border: 0;
+      border-radius: 21px;
+      background-color: white;
+      cursor: pointer;
+      outline: none;
+    }
+
+    .header-sign-up-button {
+      margin-right: 8px;
+      &:hover {
+        background-color: ${palette.gray_f7};
+      }
+    }
+
+    .header-login-button {
+      box-shadow: 0 1px 2px rgba(0, 0, 0, 0.18);
+      &:hover {
+        box-shadow: 0 2px 8px rgba(0, 0, 0, 0.12);
+      }
+    }
+  }
+`;
+
+const Header: React.FC = () => {
+  const { openModal, ModalPortal } = useModal();
+
+  return (
+    <Container>
+      <Link href='/'>
+        <a className='header-logo-wrapper'>
+          <AirbnbLogoIcon className='header-logo' />
+          <AirbnbLogoTextIcon />
+        </a>
+      </Link>
+      <div className='header-auth-buttons'>
+        <button
+          type='button'
+          className='header-button header-sign-up-button'
+          onClick={openModal}
+        >
+          회원가입
+        </button>
+        <button type='button' className='header-button header-login-button'>
+          로그인
+        </button>
+      </div>
+      <ModalPortal>
+        <SignUpModal />
+      </ModalPortal>
+    </Container>
+  );
+};
+
+export default Header;
