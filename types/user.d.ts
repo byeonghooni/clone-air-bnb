c07b91ed2edbc82d2c452dc85b6b0144922411c2
Byeonghoon Lee <byeonghoonlee@Byeonghoonui-iMac.local>
2021-02-16 15:41:04 +0900

    feat add redux functions

--- types/user.d.ts
+++ types/user.d.ts
@@ -1,8 +1,8 @@
 export type UserType = {
   id: number;
   email: string;
-  firstname: string;
-  lastname: string;
+  firstName: string;
+  lastName: string;
   birthday: string;
   profileImage: string;
 };
@@ -12,8 +12,8 @@ export type StoredUserType = {
   id: number;
   email: string;
   password: string;
-  firstname: string;
-  lastname: string;
+  firstName: string;
+  lastName: string;
   birthday: string;
   profileImage: string;
 };
